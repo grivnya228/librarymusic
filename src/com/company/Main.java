782192472112b0fd8f62110e8f34b500653772e8
Polina Matskevych <polina.matskevych@ab-soft.net>
2022-05-31 19:06:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+/**
+ * A program that prints hello world
+ *
+ * version b0.01
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
